66441d64a1e1b810755e65a64f744099c1cbc11a
usm-alina <usmanova.alina19@email.com>
2026-09-17 15:12:32 +0300

    после правок

--- class_time.cpp
+++ class_time.cpp
@@ -1,15 +1,26 @@
 #include <compare>
+#include <cmath>
 
 class Time {
-public:
     double jdInt_ = 0.0; //целые сутки!!!!
     double jdFrac_ = 0.0;
+public:
     Time(double jdInt = 0, double jdFrac = 0) noexcept
-        : jdInt_(jdInt), jdFrac_(jdFrac) {}
+        : jdInt_(jdInt), jdFrac_(jdFrac) 
+    {
+        double extra = std::floor(jdFrac_);
+        jdInt_  += extra;
+        jdFrac_ -= extra;
+
+        if (jdFrac_ < 0.0) {
+            jdFrac_ += 1.0;
+            jdInt_  -= 1.0;
+        }
+    }
     
     static Time fromJD (double jd) noexcept {
-        double i = static_cast<long long>(jd); //целую часть выделить
-        return Time(i, jd - i);
+        double i = std::floor(jd);
+        return Time(i, jd - i);  
     }
 
     static Time fromMJD (double mjd) noexcept {
@@ -25,40 +36,16 @@ public:
 
 };
 
-double operator -(const Time& first, const Time& second) noexcept { 
-        double dInt  = first.jdInt_  - second.jdInt_;
-        double dFrac = first.jdFrac_ - second.jdFrac_;
-        return (dInt + dFrac) * 86400.0; // в секундах!!
+double operator-(const Time& a, const Time& b) noexcept {
+    return (a.jd() - b.jd()) * 86400.0;
 }
 
-Time operator -(const Time& first, double secs) noexcept {
-    double days  = secs / 86400.0;
-    double dInt  = static_cast<long long>(days);
-    double dFrac = days - dInt;
-
-    Time dif;
-    dif.jdInt_  = first.jdInt_  - dInt;
-    dif.jdFrac_ = first.jdFrac_ - dFrac;
-
-    if (dif.jdFrac_ < 0.0) {
-        dif.jdFrac_ += 1.0;
-        dif.jdInt_  -= 1.0;
-    }
-    return dif;
+Time operator-(const Time& t, double secs) noexcept {
+    double days = secs / 86400.0;
+    return Time(t.jdInt(), t.jdPart() - days);
 }
 
-Time operator +(const Time& first, double secs) noexcept {
-    double days  = secs / 86400.0;
-    double dInt  = static_cast<long long>(days);
-    double dFrac = days - dInt;
-
-    Time sum;
-    sum.jdInt_  = first.jdInt_  + dInt;
-    sum.jdFrac_ = first.jdFrac_ + dFrac;
-
-    if (sum.jdFrac_ >= 1.0) {
-        sum.jdFrac_ -= 1.0;
-        sum.jdInt_  += 1.0;
-    }
-    return sum;
+Time operator+(const Time& t, double secs) noexcept {
+    double days = secs / 86400.0;
+    return Time(t.jdInt(), t.jdPart() + days);
 }
